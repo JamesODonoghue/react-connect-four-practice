7e579dea005daf6f93f1f53db448a725fd065c23
James O'Donoghue <jodonogh1@gmail.com>
2020-01-16 20:53:55 -0800

    added the connect four board, styling. added logic for displaying each player and checking for horizontal winner.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,152 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Player
+
+const PLAYER = {
+	NONE: null,
+	ONE: 1,
+	TWO: 2
+}
+// Board
+
+const initBoard = () => {
+	let board = [];
+
+	for (let i = 0; i < 42; i++) {
+		board.push(PLAYER.NONE);
+	}
+	return board;
 }
 
-export default App;
+const getLowestAvailableCell = (board, column) => {
+	for (let i = 35 + column; i >= 0; i -= 7) {
+		if (board[i] === PLAYER.NONE) {
+			console.log(i);
+			return i;
+		}
+	}
+	return -1;
+}
+
+const getPlayerNameString = (player) => {
+	return player === PLAYER.ONE ? "playerOne" : player === PLAYER.TWO ? "playerTwo" : "none"
+}
+
+const toggleCurrentPlayer = (currPlayer) => {
+	return currPlayer === PLAYER.ONE ? PLAYER.TWO : PLAYER.ONE;
+}
+
+const checkGameStatus = (board) => {
+
+	// check for horizontal winner
+
+	for (let r = 0; r < 6; r++) {
+		for (let c = 0; c <= 4; c++) {
+			let index = r * 7 + c;
+			const slice = board.slice(index, index + 4);
+			const winningResult = checkForWinningSlice(slice)
+
+			if (winningResult !== false) return winningResult;
+		}
+	}
+
+	// check for vertical winner
+
+	// for (let r = 0; r < 6; r++) {
+	// 	for (let c = 0; c <= 4; c++) {
+	// 		let index = r * 7 + c;
+	// 		const slice = board.slice(index, index + 4);
+	// 		const winningResult = checkForWinningSlice(slice)
+
+	// 		if (winningResult !== false) return winningResult;
+	// 	}
+	// }
+	// check for diagonal winner
+
+}
+
+const checkForWinningSlice = (smallBoard) => {
+	if (smallBoard.some(cell => cell === PLAYER.NONE)) {
+		return false;
+	}
+
+	if (
+		smallBoard[0] === smallBoard[1] &&
+		smallBoard[1] === smallBoard[2] &&
+		smallBoard[2] === smallBoard[3]
+	) {
+		console.log(smallBoard[1])
+		return smallBoard[1];
+	}
+
+	return false
+}
+
+class ConnectFourApp extends React.Component {
+
+	/** State */
+
+
+	/** Methods */
+
+
+	constructor() {
+		super();
+
+		this.state = {
+			board: initBoard(),
+			currentPlayer: PLAYER.ONE
+		}
+	}
+
+	makeMove(column) {
+		const { board, currentPlayer } = this.state;
+		const index = getLowestAvailableCell(board, column);
+
+		const newBoard = [...board];
+
+		newBoard[index] = currentPlayer;
+
+		const gameState = checkGameStatus(newBoard);
+
+		this.setState({
+			board: newBoard,
+			currentPlayer: toggleCurrentPlayer(currentPlayer),
+			gameState
+		})
+	}
+
+	handleCellClick = (index) => () => {
+		let column = index % 7;
+		this.makeMove(column);
+	}
+
+	renderCells() {
+		const { board } = this.state;
+		return board.map((player, index) => this.renderCell(player, index))
+	}
+
+	renderCell(player, index) {
+		return (
+			<div
+				className="board-cell"
+				data-player={getPlayerNameString(player)}
+				key={index}
+				onClick={this.handleCellClick(index)}>
+			</div>
+		)
+	}
+
+	render() {
+		return (
+			<div className="App">
+				<header className="App-header">Connect 4</header>
+				<div className="board">
+					{this.renderCells()}
+				</div>
+			</div>
+		)
+	}
+}
+export default ConnectFourApp;
